fb2c36828d9ba1593d475b520593f055a1ad7e8e
ACMattos <acmattos@gmail.com>
2024-03-23 12:40:26 -0300

    first commit

---
+++ ai/chatgpt/chatgpt-api/chatgpt-api/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+}
+
+dependencies {
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+}
+
+repositories {
+    mavenCentral()
+}
