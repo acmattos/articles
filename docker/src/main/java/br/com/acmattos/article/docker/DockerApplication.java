1d4d2f8399772feb00963830ee75afda22710d63
acmattos <acmattos@gmail.com>
2025-11-21 09:50:04 -0300

    adding enricher

---
+++ docker/src/main/java/br/com/acmattos/article/docker/DockerApplication.java
@@ -0,0 +1,13 @@
+package br.com.acmattos.article.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class DockerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApplication.class, args);
+	}
+}
